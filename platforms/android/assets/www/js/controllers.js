d2781de4689434f70d7bceb556e9daf6bb1e85dc
kstoriadis <federico.gonzalezc@gmail.com>
2016-06-25 19:18:20 -0300

    Carpeta platforms
    Para unificar

---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,33 @@
+angular.module('starter.controllers', [])
+
+.controller('MapCtrl', function($scope, $ionicLoading) {
+  $scope.mapCreated = function(map) {
+    $scope.map = map;
+  };
+
+  $scope.centerOnMe = function () {
+    console.log("Centering");
+    if (!$scope.map) {
+      return;
+    }
+
+    $scope.loading = $ionicLoading.show({
+      content: 'Getting current location...',
+      showBackdrop: false
+    });
+
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      console.log('Got pos', pos);
+      
+      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      var marker = new google.maps.Marker({
+      map: $scope.map,
+      animation: google.maps.Animation.DROP,
+      position: pos.coords
+  });
+      $scope.loading.hide();
+    }, function (error) {
+      alert('Unable to get location: ' + error.message);
+    });
+  };
+});
