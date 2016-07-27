c40be6917cdca85f80c02fb1eed78c17a1ed83dd
gcigarran <gcigarran@gmail.com>
2016-07-26 23:43:26 -0300

    Tarjeta Promocional - Implementacion de Service de Get/Update

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives', 'app.controllers.medioPago','app.services.ubicaciones','app.controllers.ubicaciones','app.controllers.datosAdicionales','app.controllers.agregarTarjetaPromocional','app.services.datosAdicionales','app.services.mediosDePago'])
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives', 'app.controllers.medioPago','app.services.ubicaciones','app.controllers.ubicaciones','app.controllers.datosAdicionales','app.controllers.agregarTarjetaPromocional','app.services.datosAdicionales','app.services.mediosDePago','app.services.tarjetaPromocional'])
 
 .run(function($ionicPlatform, BaseLocal) {
   $ionicPlatform.ready(function() {
