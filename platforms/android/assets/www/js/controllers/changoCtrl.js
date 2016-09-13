db819973c120cfb730ae6a8d5cf74e9ca4f6b2a9
kstoriadis <federico.gonzalezc@gmail.com>
2016-09-13 11:43:18 -0300

    Solo por las dudas

--- platforms/android/assets/www/js/controllers/changoCtrl.js
+++ platforms/android/assets/www/js/controllers/changoCtrl.js
@@ -1,17 +1,27 @@
 angular.module('app.controllers.chango', [])
-.controller('miChangoCtrl', function($scope,ComprarService,EscannerService) {
+.controller('miChangoCtrl', function($scope,ComprarService,EscannerService, ProductoService,ComprarService) {
 
+	var comercio = ComprarService.comercioSeleccionado();
+	
  $scope.$on("$ionicView.beforeEnter", function(event, data){
 		 
 	 $scope.listaActual = ComprarService.listaSeleccionada();
 	 $scope.nuevoProducto = '123';
  });
  
- 
+ /*
+  * Levanta el código de barras con EscannerService.scanBarcode()
+  * Busca el producto en la base interna con ProductoService.getProductoPorEAN
+  * 
+  * */
  $scope.escannear = function(){
 	 EscannerService.scanBarcode().then(function(codigo){
-		 $scope.nuevoProducto =codigo;
-		 $scope.$apply();
+		 ProductoService.getProductoPorEAN(codigo).then(function (producto){			 
+			 $scope.nuevoProducto =producto.nombre;
+			 $scope.nuevoPrecio = obtenerPorId(producto.precios, comercio._id);
+			 $scope.$apply();
+		 });
+		 
 	 });
  }
  
