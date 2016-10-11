6fdc75014cc39fd483a9be05746817518bcc8754
gcigarran <gcigarran@gmail.com>
2016-10-10 23:02:31 -0300

    Se agregan mensajes informativos respecto del fallo de registración del usuario.

--- www/js/controllers/registrarseCtrl.js
+++ www/js/controllers/registrarseCtrl.js
@@ -5,15 +5,15 @@ angular.module('app.controllers.registrarse', [])
 
     $scope.currentUser = LoginService.getCurrentUser();
     $scope.data = {};
+    
 
     $scope.createUser = function() {
+        $scope.errores = [];
         if (validarUsuario()){
             alert('Usuario creado!');
             LoginService.setCurrentUser($scope.currentUser);
             $state.go('informaciNDeUsuario')
-        } else {
-            alert('No se pudo crear el usuario, verifique que la dirección de e-mail sea correcta,\n y que la contraseña contenga al menos una mayúscula, una minúscula y 2 números.');
-        }
+        } 
 
     }
 
@@ -25,7 +25,10 @@ angular.module('app.controllers.registrarse', [])
 
     var validarMail = function() {
         // Al tratarse de un input-type="email", se valida al momento de ingresarlo que se trate de un mail, dejando el campo vacío si no lo es.
-        // Podrían surgir validaciones futuras respecto del mail.
+        // Podrían surgir validaciones futuras respecto del mail, como validar contra el backend que no esté en uso.
+        if (!$scope.currentUser.email) {
+            $scope.errores.push("Debe ingresar una dirección de e-mail válida.")
+        }
         return $scope.currentUser.email;
     }
 
@@ -35,7 +38,9 @@ angular.module('app.controllers.registrarse', [])
         //•	Se distingue entre mayúsculas y minúsculas.
         //•	Debe contener un mínimo de 2 números.
         //•	Debe contener al menos una minúscula y una mayúscula.
-        
+        if (password != $scope.data.passwordConfirm) {
+            $scope.errores.push("Las contraseñas deben coincidir entre sí.")
+        }
 
         return  validarLongitud(password) 
                 && validarMayusculasMinusculas(password)
@@ -44,10 +49,21 @@ angular.module('app.controllers.registrarse', [])
     }
 
     var validarLongitud = function(texto) {
+        if (texto.length < 6 || texto.length > 10) {
+            $scope.errores.push("La contraseña debe tener entre 6 y 10 caracteres.");
+        }
         return texto.length > 5 && texto.length < 11;
     }
 
     var validarMayusculasMinusculas = function(texto) {
+        if (texto.toUpperCase() == texto) {
+            $scope.errores.push("La contraseña debe tener al menos una minúscula.")
+        } 
+
+        if (texto.toLowerCase() == texto) {
+            $scope.errores.push("La contraseña debe tener al menos una mayúscula.")
+        }
+
         return  (texto.toUpperCase() != texto)
                 && (texto.toLowerCase() != texto)
     }
@@ -60,6 +76,10 @@ angular.module('app.controllers.registrarse', [])
             }
                 
         }
+        if (cantNumeros < 2) {
+            $scope.errores.push("La contraseña tiene menos de 2 números.")
+        }
+
         return cantNumeros > 1;
     }
 
